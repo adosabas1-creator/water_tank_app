8f70a233b8e172128ff20a508bc5fda3c9fc73aa
Mho2010 <adosabas1@gmail.com>
2026-09-15 20:05:30 +0300

    Bind logged-in user to service authorization

--- lib/core/auth/user_provider.dart
+++ lib/core/auth/user_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import '../../models/user.dart';
+import 'permission_service.dart';
 
 class UserProvider extends ChangeNotifier {
   User? _currentUser;
@@ -7,11 +8,13 @@ class UserProvider extends ChangeNotifier {
 
   void setUser(User? user) {
     _currentUser = user;
+    PermissionService.setCurrentUser(user);
     notifyListeners();
   }
 
   void logout() {
     _currentUser = null;
+    PermissionService.setCurrentUser(null);
     notifyListeners();
   }
 }
